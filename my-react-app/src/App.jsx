715b96a4b27e7699b96c84320896d7a133591dd8
Joel Fernandes <fernandesjoel171819@gmail.com>
2025-07-14 22:17:42 +0530

    added files

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import ComponentA from './ComponentA.jsx';
+function App() {
+
+  return(<>
+         <ComponentA/>
+         <ComponentA/>
+         <ComponentA/>
+         <ComponentA/>
+  </>);
+  
+  }
+export default App
